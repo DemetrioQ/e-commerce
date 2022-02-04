37e8624f715775883698025345190dccda37f2d1
Demetrio Quiñones <demetriorqe@gmail.com>
2022-02-03 20:58:24 -0400

    e-commerce complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
-
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
 
@@ -37,6 +38,24 @@ const App = () => {
     setCart(cart);
   };
 
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+      console.log(errorMessage)
+    }
+  };
+
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -50,7 +69,7 @@ const App = () => {
           <Route path='/' exact element={<Products products={products} onAddToCart={handleAddToCart} />} />
 
           <Route path='/cart' exact element={<Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart} />} />
-          <Route path='/checkout' exact element={<Checkout cart={cart} />} />
+          <Route path='/checkout' exact element={<Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage}/>} />
         </Routes>
       </div>
     </Router>
--- src/components/CheckoutForm/FormInput.jsx
+++ src/components/CheckoutForm/FormInput.jsx
@@ -4,9 +4,11 @@ import { useFormContext, Controller } from 'react-hook-form';
 
 const FormInput = ({ name, label }) => {
   const { control } = useFormContext();
+  const isError = false;
+  
   return (
     <Grid item xs={12} sm={6}>
-      <Controller as={TextField} control={control} fullWidth name={name} render={({ field }) => <TextField fullWidth label={label} required />} />
+      <Controller as={TextField} defaultValue='' name={name} control={control} label={label} fullWidth required error={isError} />
     </Grid>
   );
 };
